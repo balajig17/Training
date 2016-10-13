8e227603fbd7e2ef232a8371e6aaf47836122095
Balaji Gurumurthy <balajig17@gmail.com>
2016-10-12 17:21:15 -0700

    Updated FlattenList code

--- src/ds/list/FlattenList.java
+++ src/ds/list/FlattenList.java
@@ -1,46 +1,85 @@
 package ds.list;
 
 public class FlattenList {
-	
 
+	private ListNode curr = null;
+	private ListNode head;
+
+	public FlattenList() {
+		head = new ListNode('0');
+		curr = head;
+	}
 
 	public static void main(String[] args) {
 		
+		FlattenList fl = new FlattenList();
+		ListNode list = createList();
+		printList(list);
+		list = fl.flattenedList(list);
+		printList(list);
 
 	}
 	
-	public ListNode flattenedList(ListNode src) {
-		
-		ListNode result = null;
-		ListNode nextNode = null;
-		ListNode temp = src;
-		while(temp.next != null || temp.down != null) {
-			ListNode down = null;
-			if(temp.down != null) {
-				down = flattenedList(temp.down);
-			}
-			if(result == null) {
-				nextNode = new ListNode(temp.value);
-				
-			}
-			else {
-				nextNode = new ListNode(temp.value);
-			}
+	public static ListNode createList() {
+		ListNode head = new ListNode('A');
+		ListNode temp = head;
+		temp.next = new ListNode('B');
+		temp = temp.next;
+		temp.next = new ListNode('C');
+		temp = temp.next;
+		temp.next = new ListNode('D');
+		temp = temp.next;
+		temp.down = createSubList();
+		temp.next = new ListNode('E');
+		temp = temp.next;
+		temp.next = new ListNode('F');
+		return head;
+	}
+	
+	public static ListNode createSubList() {
+		ListNode head = new ListNode('G');
+		ListNode temp = head;
+		temp.next = new ListNode('H');
+		temp = temp.next;
+		temp.next = new ListNode('I');
+		temp.down = new ListNode('K');
+		temp.down.next = new ListNode('L');
+		temp = temp.next;
+		temp.next = new ListNode('J');
+		return head;
+	}
+	
+	public static void printList(ListNode head) {
+		while(head != null) {
+			System.out.println(head.value);
+			head = head.next;
 		}
-		return null;
+	}
+
+	public ListNode flattenedList(ListNode src) {
+
+		ListNode temp = src;
+		while (temp != null) {
+			if (temp.down != null) {
+				flattenedList(temp.down);
+			}
+			curr.next = new ListNode(temp.value);
+			curr = curr.next;
+			temp = temp.next;
+		}
+		return head.next;
 	}
 
 }
 
-
 class ListNode {
-	
+
 	ListNode next = null;
 	ListNode down = null;
 	char value;
-	
+
 	public ListNode(char c) {
 		this.value = c;
 	}
-	
+
 }
